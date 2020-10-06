fc22f80e0e531f83af537b3cad88d4b3bbaa69c7
filipsupolik25@gmail.com <14Filip14>
2020-10-06 09:03:47 +0200

    12a_exercise finished

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,13 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        // write your code here
+        Student student1 = new Student();
+        student1.name = "John";
+        student1.studentnumber = 12345;
+
+
+        System.out.println(student1.getName());
+        System.out.println(student1.getStudentnumber());
     }
 }
---
+++ src/com/appslab/Student.java
@@ -0,0 +1,14 @@
+package com.appslab;
+
+public class Student {
+    String name;
+    int studentnumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStudentnumber() {
+        return studentnumber;
+    }
+}
