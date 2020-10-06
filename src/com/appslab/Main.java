b3581200d74743184f7f27138f259f78a6ca9b18
filipsupolik25@gmail.com <14Filip14>
2020-10-06 09:56:23 +0200

    PersonBudget_exercise finished

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,23 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+       Person people [] = new Person[3];
+       Person people1 = new Person("John",12,500);
+       Person people2 = new Person("Steve", 28,1500);
+       Person people3 = new Person("Martin", 49, 2280);
+
+       people[0] = people1;
+       people[1] = people2;
+       people[2] = people3;
+
+       System.out.println(calculateBudged(people));
+    }
+
+    public static int calculateBudged(Person[] people) {
+        int budget = 0;
+        for (int i = 0; i< people.length; i++) {
+            budget += people[i].budget;
+        }
+        return budget;
     }
 }
