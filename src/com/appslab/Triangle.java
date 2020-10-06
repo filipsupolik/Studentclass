e15774acb2ccd04b617b75f5968f0456b0fab420
filipsupolik25@gmail.com <14Filip14>
2020-10-06 10:10:05 +0200

    Triangle_notfinished

---
+++ src/com/appslab/Triangle.java
@@ -0,0 +1,22 @@
+package com.appslab;
+
+public class Triangle {
+    int sideA;
+    int sideB;
+    int sideC;
+    int area;
+    int perimeter;
+
+
+    public int getArea() {
+        int s = perimeter/2;
+        double area1 = Math.sqrt(s-(s-sideA)*(s-sideB)*(s-sideC));
+        area = (int) area1;
+        return area;
+    }
+
+    public int getPerimeter() {
+        perimeter = sideA+sideB+sideC;
+        return perimeter;
+    }
+}
